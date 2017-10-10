54cc58fbc5239071cfcbdc72c4d9af9e9fdbdaa1
Matheus Castanho <mscastanho@gmail.com>
2017-10-10 17:25:01 -0300

    Added basic forwarding

--- sfcapp.c
+++ sfcapp.c
@@ -1,6 +1,7 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <signal.h>
 
 #include <rte_eal.h>
 #include <rte_ethdev.h>
@@ -14,6 +15,12 @@
 #define NB_TX_QS 1
 #define NB_RX_DESC 128
 #define NB_TX_DESC 128
+#define BURST_SIZE 8
+
+#define INTERCORE_RING_SIZE 64
+
+/* Remove later*/
+int n_rx=0, n_tx=0;
 
 static uint32_t sfcapp_enabled_port_mask = 0;
 
@@ -23,6 +30,12 @@ struct sfcapp_port_assoc {
     uint32_t rx_sf;
     uint32_t tx_sf; 
 };
+
+#ifdef SFCAPP_MULTICORE
+/*Rings for intercore communication*/
+static struct rte_ring *rx_ring;
+static struct rte_ring *tx_ring;
+#endif
 
 static struct rte_mempool *sfcapp_pktmbuf_pool;
 
@@ -98,6 +111,17 @@ parse_args(int argc, char **argv){
     }
 }
 
+static void
+signal_handler(int signum)
+{
+	if (signum == SIGINT || signum == SIGTERM) {
+		printf("\n\n%d packets received\n%d packets transmitted",
+				n_rx,n_tx);
+    }
+    
+    exit(0);
+}
+
 /* Function to allocate memory to be used by the application */ 
 static void
 alloc_mem(unsigned n_mbuf){
@@ -166,23 +190,74 @@ init_port(uint8_t port, struct rte_mempool *mbuf_pool){
 
     struct ether_addr eth_addr;
     rte_eth_macaddr_get(port,&eth_addr);
-    printf("MAC of port %u: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
-            " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
+    printf("MAC of port %u: %02" PRIx8 ":%02" PRIx8 ":%02" PRIx8
+            ":%02" PRIx8 ":%02" PRIx8 ":%02" PRIx8 "\n",
             (unsigned) port,
             eth_addr.addr_bytes[0],eth_addr.addr_bytes[1],
             eth_addr.addr_bytes[2],eth_addr.addr_bytes[3],
             eth_addr.addr_bytes[4],eth_addr.addr_bytes[5]);
 
+    /* Remove this later! */
+    rte_eth_promiscuous_enable(port);
+
     return 0;
 
+}
+
+#ifdef SFCAPP_MULTICORE //To be used in the future when I implement processing with 2 lcores
+static int
+init_rings(){
+
+    unsigned flags = RING_F_SC_DEQ | RING_F_SP_ENQ;
+    
+    rx_ring = rte_ring_create("rx_ring",INTERCORE_RING_SIZE,
+                rte_socket_id(),flags);
+
+    tx_ring = rte_ring_create("tx_ring",INTERCORE_RING_SIZE,
+                rte_socket_id(),flags);
+    
+    if(rx_ring == NULL || tx_ring == NULL)
+        return -1;
+    
+    return 0;
+}
+#endif
+
+static __attribute__((noreturn)) void 
+main_loop(void) {
+    int rx_port = 0;
+    int tx_port = 1;
+
+    for(;;){
+        struct rte_mbuf *rx_pkts[BURST_SIZE];
+        uint16_t nb_rx = rte_eth_rx_burst(rx_port,0,
+            rx_pkts,BURST_SIZE);
+        
+        if(unlikely(nb_rx == 0))
+            continue;
+        
+        n_rx += nb_rx;
+        
+        uint16_t nb_tx = rte_eth_tx_burst(tx_port,0,
+            rx_pkts,nb_rx);
+
+        n_tx += nb_tx;
+
+        if(unlikely(nb_tx < nb_rx)){
+            uint16_t buf;
+            for(buf = nb_tx ; buf < nb_rx ; buf++)
+                rte_pktmbuf_free(rx_pkts[buf]);
+        }
+    
+    }
 }
 
 int 
 main(int argc, char **argv){
 
     int ret;
-    int n_ports;
-    //unsigned lcore_id;
+    int nb_ports;
+    int nb_lcores;
     uint8_t port;
 
     ret = rte_eal_init(argc,argv);
@@ -196,16 +271,34 @@ main(int argc, char **argv){
  
     alloc_mem(NB_MBUF);
 
+    /* Remove later*/
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
     /* setup match table */
+ 
+    #ifdef SFCAPP_MULTICORE
+    /* Setup rings for communication between cores.
+     * lcore 0 writes received packets to rx_ring
+     * lcore 1 reads packets from rx_ring and executes encap/decap
+     * lcore 1 writes packets to tx_ring, for transmission
+     * lcore 0 reads packets from tx_ring and sends them
+     */
+    ret = init_rings();
+    if(ret < 0)
+        rte_exit(EXIT_FAILURE,"Failed to setup internal communication rings.\n");
+    
+    #endif
 
-    n_ports = rte_eth_dev_count();
+    /* setup ports */ 
+    nb_ports = rte_eth_dev_count();
 
-    if(n_ports < 2)
+    if(nb_ports < 2)
         rte_exit(EXIT_FAILURE, 
             "Not enough Ethernet ports (%d) available\n",
-            n_ports);
-
-    for(port = 0 ; port < n_ports ; port++){
+            nb_ports);
+    
+    for(port = 0 ; port < nb_ports ; port++){
         if( (sfcapp_enabled_port_mask & (1<<port)) == 0)
             continue;
         
@@ -218,10 +311,14 @@ main(int argc, char **argv){
 
     /* Assign one lcore for RX/TX and another for encap/decap tasks
      * Only one queue per port will be used.
-     */ 
-    
+     */
+    nb_lcores = rte_lcore_count();
+    if(nb_lcores < 2)
+        rte_exit(EXIT_FAILURE,"Not enough lcores! At least 2 needed.\n");
 
-    /* run main loop */
+    
+    /* start main loop on slaves */
+    
 
     return ret;
 }
