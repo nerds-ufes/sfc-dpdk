60383a3b2de1155a56363ce6b4fff7584b5622b0
Matheus Castanho <mscastanho@gmail.com>
2017-10-18 16:04:23 -0200

    Added nsh function headers

--- nsh.h
+++ nsh.h
@@ -6,7 +6,7 @@
 #define ETHER_TYPE_NSH 0x894F /* extension to rte_ether.h ether types */
 
 #define NSH_BASE_HEADER_LEN 4
-#define NSH_SERVICE_PATH_HEADER_LEN 4
+#define NSH_SPI_LEN 3
 //#define NSH_CONTEXT_HEADER_LEN ?
 
 #define NSH_SERVICE_PATH_ID_LEN 3
@@ -20,4 +20,20 @@ struct nsh_hdr {
     uint32_t *context; /* What type should I use for this? */
 } __attribute__((__packed__));
 
+struct nsh_spi {
+    uint8_t spi_bytes[NSH_SPI_LEN];
+} __attribute__((__packed__));
+
+
+/* Encapsulates the packet in pkt_mbuf in NSH header with
+ * NSH parameters given by service_header arg.
+ */
+void nsh_encap(rte_mbuf* pkt_mbuf, uint32_t service_header);
+
+/* Desencapsulates the packet in pkt_mbuf, removing the NSH
+ * header and returning a pointer to the removed header.
+ */
+rte_mbuf* nsh_decap(rte_mbuf* pkt_mbuf);
+
+void nsh_dec_si(rte_mbuf* pkt_mbuf);
 #endif
