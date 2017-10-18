793608251542933b4186427b297cdf6b66052b35
Matheus Castanho <mscastanho@gmail.com>
2017-10-18 16:03:46 -0200

    Adding new app types files

---
+++ sfc_classifier.h
@@ -0,0 +1,22 @@
+﻿#ifndef SFCAPP_CLASSIFIER_
+#define SFCAPP_CLASSIFIER_
+
+#include <rte_hash.h>
+
+#define CLASSIFIER_TABLE_SZ 1024
+
+/* static rte_hash* classifier_flow_path_lkp_table*/
+/* key = ipv4_5tuple ; value = nsh_spi */
+
+/* static rte_hash* classifier_sfp_lkp_table */
+/* key = spi (nsh_spi) ; value = chain length (uint8_t) */
+
+int classifier_init(char** cfg_filename);
+
+/* static classifier_parse_config_file(char** cfg_filename); */
+
+static __attribute__((noreturn)) void 
+classifier_main_loop(void);
+
+
+#endif
---
+++ sfc_forwarder.h
@@ -0,0 +1,22 @@
+﻿#ifndef SFCAPP_FORWARDER_
+#define SFCAPP_FORWARDER_
+
+#include <rte_hash.h>
+
+#define FORWARDER_TABLE_SZ 1024
+
+/* static rte_hash* forwarder_next_func_lkp_table*/
+/* key = spi-si ; value = next sf_id (uint16_t) */
+
+/* static rte_hash* forwarder_next_hop_lkp_table */
+/* key = sf_id (uint16_t) ; value = chain length (uint8_t) */
+
+int forwarder_init(char** cfg_filename);
+
+/* static forwarder_parse_config_file(char** cfg_filename); */
+
+static __attribute__((noreturn)) void 
+forwarder_main_loop(void);
+
+
+#endif
