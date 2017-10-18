0b606c03732796d3af304577e2979a36654379ee
Matheus Castanho <mscastanho@gmail.com>
2017-10-18 16:05:13 -0200

    Added new global configs

--- sfc_common.h
+++ sfc_common.h
@@ -1,6 +1,8 @@
 ﻿#ifndef SFCAPP_COMMON_
 #define SFCAPP_COMMON_
 
+#include <rte_cfgfile.h>
+
 struct ipv4_5tuple {
     uint8_t proto;
     uint16_t src_ip;
@@ -19,7 +21,12 @@ struct sfcapp_config {
     uint8_t rx_port;
     uint8_t tx_port;
     enum sfcapp_type type;
-    
+    void (*main_loop)(void*);
+    /* struct sfcapp_stats stats; */  
 };
+
+struct rte_cfgfile_parameters sfcapp_cfgfile_parameters = {
+    .comment_character = '#';
+}
 
 #endif
