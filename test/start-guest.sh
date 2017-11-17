9911e8aead5900097cf965736e640f742d7d52d3
Matheus Castanho <mscastanho@gmail.com>
2017-11-17 16:02:40 -0200

    Added scripts for general testing

--- test/start-guest.sh
+++ test/start-guest.sh
@@ -18,13 +18,15 @@ rm -d /tmp/qemu_share$id
 mkdir /tmp/qemu_share$id
 
 echo "Starting $name..."
-cmd_base="qemu-system-x86_64 -cpu host -boot c -drive file=$path,media=disk,format=raw -m 1048M -smp cores=2 --enable-kvm -name $name -vnc :$id -pidfile /tmp/vm_$id.pid  -drive file=fat:rw:/tmp/qemu_share$id,snapshot=off -monitor unix:/tmp/vm_$idmonitor,server,nowait"
+cmd_base="qemu-system-x86_64 -cpu host -boot c -drive file=$path,media=disk,format=raw -m 1024M -smp cores=2 --enable-kvm -name $name -vnc :$id -pidfile /tmp/vm_$id.pid  -drive file=fat:rw:/tmp/qemu_share$id,snapshot=off -monitor unix:/tmp/vm_$idmonitor,server,nowait"
 
 echo "low: $low_idx high: $high_idx"
 
 for i in $( seq $low_idx 1 $high_idx );
 do
-	cmd_dev="$cmd_dev-chardev socket,id=char$i,path=/usr/local/var/run/openvswitch/vhost-user$i -netdev type=vhost-user,id=mynet$i,chardev=char$i,vhostforce -device virtio-net-pci,mac=00:00:00:00:00:$i,netdev=mynet$i,id=net$i"
+	mac=$(printf "00:00:00:00:00:%02x" $i)
+	echo $mac
+	cmd_dev="$cmd_dev -chardev socket,id=char$i,path=/usr/local/var/run/openvswitch/vhost-user$i -netdev type=vhost-user,id=mynet$i,chardev=char$i,vhostforce -device virtio-net-pci,mac=$mac,netdev=mynet$i,id=net$i"
 done
 
 cmd_end="-object memory-backend-file,id=mem,size=1024M,mem-path=/mnt/huge,share=on -numa node,memdev=mem -mem-prealloc &"
